f3f723b5eb308a5d00e1786a71764aa14f705ff5
wardrush <wardrushton@gmail.com>
2018-06-05 14:42:40 -0500

    Ch 11 Facelift Added CSS bootstrap functionality Will modify later to personalize

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 # for email
 from flask_mail import Mail
@@ -16,6 +17,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
--- config.py
+++ config.py
@@ -11,28 +11,13 @@ class Config(object):
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USE_SSL=False
+    MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = MAIL_USERNAME
-    MAIL_MAX_EMAILS=None
+    MAIL_MAX_EMAILS = None
     POSTS_PER_PAGE = 25
     ADMINS = ['james.e.b.rushton@gmail.com']
-    """
-    ADMINS = ['james.e.b.rushton@gmail.com']
-    POSTS_PER_PAGE = 25
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    #MAIL_SERVER = 'smtp.gmail.com'
-    #MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS=True
-    MAIL_USE_SSL=False
-    MAIL_USERNAME='james.e.b.rushton@gmail.com'
-    MAIL_PASSWORD='Kunststoff44'
-    MAIL_DEFAULT_SENDER = MAIL_USERNAME
-    MAIL_MAX_EMAILS=None
-    # MAIL_SUPPRESS_SEND=  app.testing
-    # MAIL_ASCII_ATTACHMENTS: default
-    """
 
 
 
